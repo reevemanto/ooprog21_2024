e33ab9a041ff1946be413e0e1afd01e7bef78786
reevemanto <christianmanto2004@gmail.com>
2024-09-02 21:37:33 +0800

    Create SammysRentalPrice.java

---
+++ Chapter 2/SammysRentalPrice.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class sammyseashoresupplies{
+
+   public static void main(String[] args){
+   double COST_PER_HOUR = 40.00;
+   double COST_PER_MINUTE = 1.00;
+   
+   Scanner scanner = new Scanner(System.in);
+   System.out.println("Welcome to Sammy's Seashore Supplies: Your fun in the sun starts here! ");
+   
+   System.out.print("Enter the number of minutes rented a piece of sports equipment: ");
+   int minutesRented = scanner.nextInt();
+   
+   int hours = minutesRented / 60;
+   int minutes = minutesRented % 60;
+   double totalPrice = (hours * COST_PER_HOUR) + (minutes * COST_PER_MINUTE);
+   
+   System.out.println("Hours rented: "+ hours);
+   System.out.println("minute rented: "+ minutes);
+   System.out.println("The Total Price: "+ totalPrice);
+   }
+}
