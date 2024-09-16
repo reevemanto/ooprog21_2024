d9634af7a5124e866e3f6ad7510c2a1190bb3440
reevemanto <christianmanto2004@gmail.com>
2024-09-16 20:20:31 +0800

    Create FirstAddressExcercise.java

---
+++ Chapter 2/FirstAddressExcercise.java
@@ -0,0 +1,23 @@
+class FirstAddressExcercise{
+public static void main(String[] args){
+      
+      System.out.println("First Java application ");
+      displayAddress();   
+   }
+  
+   public static void displayAddress(){
+   
+      System.out.println("XYZ Company ");
+     
+      System.out.println("8900 U.S Hwy 14");
+      
+      System.out.println("Crystal Lake, IL 60014 ");
+      
+      System.out.println("Company: Apple Inc. ");
+
+   }
+ }
+
+//What is the purpose of the main method in this program?The purpose of the main method is to serve as the starting point where the program begins execution.
+//How does the displayAddress() method contribute to the overall output of the program?The displayAddress() method contributes to the overall output of the program by displaying the address information when it is called.
+//What would happen if you removed the System.out.println("First Java application"); line from the main method?If you removed the System.out.println("First Java application"); line from the main method, the message "First Java application" would not be printed but the program would still run normally without  the output.
